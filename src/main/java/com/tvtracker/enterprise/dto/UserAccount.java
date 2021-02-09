a6cda1790bb475c4c9d12d0709e2af53aa2e0b41
tate <tatettw@live.com>
2021-02-08 19:19:05 -0500

    Add project packages, dto classes, and service interfaces

---
+++ src/main/java/com/tvtracker/enterprise/dto/UserAccount.java
@@ -0,0 +1,60 @@
+package com.tvtracker.enterprise.dto;
+
+import java.sql.Timestamp;
+
+public class UserAccount {
+    private String username;
+    private String password;
+    private String email;
+    private Timestamp birthDate;
+    private Timestamp lastLogin;
+    private String token;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Timestamp getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Timestamp birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Timestamp getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Timestamp lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
