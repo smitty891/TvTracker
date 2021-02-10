15c3b33853a5ade836bcee6ff10f2de776dff080
tate <tatettw@live.com>
2021-02-09 20:53:16 -0500

    add javadoc annos to TvTrackerController

--- src/main/java/com/tvtracker/enterprise/TvTrackerController.java
+++ src/main/java/com/tvtracker/enterprise/TvTrackerController.java
@@ -15,11 +15,31 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * The controller for Tv Tracker REST endpoints and web UI.
+ *
+ * <p>
+ *     This class handles the CRUD operations for Tv Tracker's user accounts and media entries, via HTTP actions.
+ * </p>
+ * <p>
+ *     This class also serves HTML based web pages for UI interactions.
+ * </p>
+ */
 @Controller
 public class TvTrackerController {
     IUserAccountService userAccountService = new UserAccountServiceStub();
     IMediaEntryService mediaEntryService = new MediaEntryServiceStub();
 
+    /**
+     * Create a new user account record from the user account data provided.
+     *
+     * Returns one of the following status codes:
+     * 201: successfully created a user account.
+     * 409: unable to create a user account, because username already exists in the database.
+     *
+     * @param userAccount a JSON representation of a UserAccount object
+     * @return a valid user token for session authentication
+     */
     @PostMapping(value="/signUp", consumes="application/json", produces="application/json")
     public ResponseEntity signUpUser(@RequestBody UserAccount userAccount){
         HttpHeaders headers = new HttpHeaders();
@@ -34,6 +54,17 @@ public class TvTrackerController {
         }
     }
 
+    /**
+     * Authenticate user and return newly generated session token
+     *
+     * Returns one of the following status codes:
+     * 201: successfully authenticated user.
+     * 401: invalid username password combination.
+     *
+     * @param username String uniquely identifying a user
+     * @param password String that authenticates a user
+     * @return a valid user token for session authentication
+     */
     @GetMapping("/authenticate/{username}/{password}")
     public ResponseEntity authenticateUser(@PathVariable("username") String username, @PathVariable("password") String password){
         HttpHeaders headers = new HttpHeaders();
@@ -49,12 +80,28 @@ public class TvTrackerController {
         }
     }
 
+    /**
+     * Returns all media entries tied to the given username
+     *
+     * @param username String uniquely identifying a user
+     * @return List user's media entries
+     */
     @GetMapping("/getMediaEntries/{username}")
     @ResponseBody
     public List<MediaEntry> getUsersMediaEntries(@PathVariable("username") String username) {
         return mediaEntryService.fetchMediaEntriesByUsername(username);
     }
 
+    /**
+     * Update an existing media entry record
+     *
+     * Returns one of the following status codes:
+     * 201: successfully updated media entry.
+     * 400: failed to update media entry.
+     *
+     * @param mediaEntry a JSON representation of a MediaEntry object
+     * @return HttpStatus
+     */
     @PutMapping(value="/editMediaEntry", consumes="application/json", produces="application/json")
     public ResponseEntity editMediaEntry(@RequestBody MediaEntry mediaEntry){
         HttpHeaders headers = new HttpHeaders();
@@ -69,6 +116,16 @@ public class TvTrackerController {
         }
     }
 
+    /**
+     * Delete media entry record
+     *
+     * Returns one of the following status codes:
+     * 201: successfully deleted media entry.
+     * 400: failed to delete media entry.
+     *
+     * @param entryId integer uniquely identifying the media entry record
+     * @return HttpStatus
+     */
     @DeleteMapping("/removeMediaEntry/{entryId}")
     public ResponseEntity removeMediaEntry(@PathVariable("entryId") int entryId){
         HttpHeaders headers = new HttpHeaders();
@@ -83,8 +140,18 @@ public class TvTrackerController {
         }
     }
 
+    /**
+     * Create a new media entry record
+     *
+     * Returns one of the following status codes:
+     * 201: successfully created media entry.
+     * 400: failed to create media entry.
+     *
+     * @param mediaEntry a JSON representation of a MediaEntry object
+     * @return HttpStatus
+     */
     @PostMapping(value="/addMediaEntry", consumes="application/json", produces="application/json")
-    public ResponseEntity mediaMediaEntry(@RequestBody MediaEntry mediaEntry){
+    public ResponseEntity addMediaEntry(@RequestBody MediaEntry mediaEntry){
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
