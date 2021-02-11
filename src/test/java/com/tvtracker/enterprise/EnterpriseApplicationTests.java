0ae6dc94cbad46dd8787183529b9cbc0024a36f4
tate <tatettw@live.com>
2021-02-10 19:44:32 -0500

    added a test case for updating a media entry

--- src/test/java/com/tvtracker/enterprise/EnterpriseApplicationTests.java
+++ src/test/java/com/tvtracker/enterprise/EnterpriseApplicationTests.java
@@ -1,13 +1,71 @@
 package com.tvtracker.enterprise;
 
+import com.tvtracker.enterprise.dto.MediaEntry;
+import com.tvtracker.enterprise.service.IMediaEntryService;
+import com.tvtracker.enterprise.service.IUserAccountService;
+import com.tvtracker.enterprise.service.MediaEntryServiceStub;
+import com.tvtracker.enterprise.service.UserAccountServiceStub;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 @SpringBootTest
 class EnterpriseApplicationTests {
+
+    IUserAccountService userAccountService = new UserAccountServiceStub();
+    IMediaEntryService mediaEntryService;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void userUpdatesMediaEntry_ReturnsSuccessBoolean() {
+        // start with fresh media entry collection
+        mediaEntryService = new MediaEntryServiceStub();
+
+        givenUserHasRetrievedMediaEntries();
+        int entryId = whenUserUpdatesMediaEntry();
+        returnIndicationOfSuccess(entryId);
+    }
+
+    private void givenUserHasRetrievedMediaEntries() {
+        List<MediaEntry> mediaEntries = mediaEntryService.fetchMediaEntriesByUsername("testUser");
+
+        Assert.notNull(mediaEntries, "User's collection of media entries was null.");
+        Assert.noNullElements(mediaEntries, "A null element was found in user's collection of media entries.");
+        Assert.notEmpty(mediaEntries, "User's collection of media entries was empty.");
+    }
+
+    private int whenUserUpdatesMediaEntry() {
+        List<MediaEntry> mediaEntries = mediaEntryService.fetchMediaEntriesByUsername("testUser");
+        // update a media entry
+        MediaEntry mediaEntry = mediaEntries.get(0);
+        MediaEntry newMediaEntry = new MediaEntry();
+        newMediaEntry.setEntryId(mediaEntry.getEntryId());
+        newMediaEntry.setUsername(mediaEntry.getUsername());
+        newMediaEntry.setTitle(mediaEntry.getTitle());
+        newMediaEntry.setType(mediaEntry.getType());
+        newMediaEntry.setDescription("Terrible");
+        newMediaEntry.setPlatform("Hulu");
+        newMediaEntry.setWatched(true);
+
+        boolean success = mediaEntryService.updateMediaEntry(newMediaEntry);
+        Assert.isTrue(success, "An error occurred while updating a media entry.");
+
+        return newMediaEntry.getEntryId();
+    }
+
+    private void returnIndicationOfSuccess(int entryId) {
+        // check if media entry was successfully updated
+        List<MediaEntry> mediaEntries = mediaEntryService.fetchMediaEntriesByUsername("testUser");
+        MediaEntry foundEntry = null;
+        for(MediaEntry entry: mediaEntries) if (entry.getEntryId() == entryId) foundEntry = entry;
+
+        Assert.notNull(foundEntry, "The updated media entry was not found.");
+        boolean updated = foundEntry.isWatched() && foundEntry.getPlatform().equals("Hulu") && foundEntry.getDescription().equals("Terrible");
+        Assert.isTrue(updated, "Media entry's fields were not updated.");
+    }
 }
