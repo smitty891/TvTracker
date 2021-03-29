c354fdb03c091ab05e1e9ed340ae6aa8123f86f6
smitty891 <smitty891@users.noreply.github.com>
2021-03-28 20:20:08 -0400

    Establish logback spring configuration file

--- src/main/java/com/tvtracker/enterprise/EnterpriseApplication.java
+++ src/main/java/com/tvtracker/enterprise/EnterpriseApplication.java
@@ -2,10 +2,9 @@ package com.tvtracker.enterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
+
 public class EnterpriseApplication {
 
     public static void main(String[] args) {
